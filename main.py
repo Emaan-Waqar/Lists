45417beed264f46f7b22e305be68ccd4cb324b2d
Emaan Waqar <149870059+Emaan-Waqar@users.noreply.github.com>
2025-09-17 16:31:22 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def square(start, end):
+    even= []
+    odd= []
+    for number in range(start, end+1):
+        sq= sq**2
+
+    if sq %2 ==0:
+        even.append(num)
+num= int(input("Enter the number you want to find square of: "))
+square(1,10)
